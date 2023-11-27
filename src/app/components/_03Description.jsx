e98ce755f55294e0e03c80549c143013a472c486
Jonatan Missora <jonatanjmissora@gmail.com>
2023-11-27 11:34:53 -0300

    lu correccion 27-11 uno

--- src/app/components/_03Description.jsx
+++ src/app/components/_03Description.jsx
@@ -13,7 +13,7 @@ export const _03Description = () => {
           className={styles.description}
           options={{from: "bottom", amount:"50", margin: "-30", delay: "0"}}
         >
-          <h3 className={styles.title}>SALÓN</h3>
+          <h3 className={styles.title}>Salón</h3>
           <p>Con una capacidad para 200 invitados, contamos con mesas y sillas Tiffany, vajillas completas, ambientes totalmente climatizados, cuatro livings exteriores, dos pantallas para transmisión del evento.</p>
         </FramerReveal>
 
@@ -21,7 +21,7 @@ export const _03Description = () => {
           className={styles.description}
           options={{from: "bottom", amount:"50", margin: "-30", delay: "0.45"}}
         >
-          <h3 className={styles.title}>LOGÍSTICA</h3>
+          <h3 className={styles.title}>Logística</h3>
           <p>Brindamos total asesoramiento integral previo al evento. Servicio de atención en salón y personal de limpieza para salón y toilettes. Contamos con servicio de barra de tragos, bartender, cabina de DJ, fotografía 360, latones para bebida y mucho más.</p>
         </FramerReveal>
 
@@ -29,7 +29,7 @@ export const _03Description = () => {
           className={styles.description}
           options={{from: "bottom", amount:"50", margin: "-30", delay: "1"}}
         >
-          <h3 className={styles.title}>CENTRO al AIRE LIBRE</h3>
+          <h3 className={styles.title}>Patio al aire libre</h3>
           <p>Salón único en su clase, jardines con paisajismo en pleno centro de la ciudad, transformaran cada evento en un momento mágico. Ideal para ceremonias y celebraciones al aire libre.</p>
         </FramerReveal>
 
