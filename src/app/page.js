f334076f900c95a1ac6f61e4c85a5985902f68ef
Jonatan Missora <jonatanjmissora@gmail.com>
2023-12-06 12:42:10 -0300

    06-12 dos

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ import { _05Sociales } from './components/_05Sociales'
 import { _06Corporativos } from './components/_06Corporativos'
 import { _07Exclusivos } from './components/_07Exclusivos'
 import { _08Catering } from './components/_08Catering'
-import { _08bisGaleria } from './components/_08bisGaleria'
 import { _09Contacto } from './components/_09Contacto'
 import { useEffect } from 'react'
 
