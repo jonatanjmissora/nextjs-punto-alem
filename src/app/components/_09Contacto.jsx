ebb1867b397e06880981613bf26a9ec800e34a41
Jonatan Missora <jonatanjmissora@gmail.com>
2024-01-17 19:31:30 -0300

    17-01 dos

--- src/app/components/_09Contacto.jsx
+++ src/app/components/_09Contacto.jsx
@@ -83,19 +83,21 @@ export const _09Contacto = () => {
           className={styles.data_item}
           options={{from: "bottom", amount:"60", margin: "-20", delay: "0"}}
         >
-          <a 
-            className={styles.icon} 
-            href="https://maps.app.goo.gl/FuTkt3hptixiTBSw5" 
-            title="maps_location" 
-            target="_blank"
-          >
-            <MapPinSvg />
-          </a>
-          <h3 className={styles.title}>Ubicanos</h3>
-          <div>
+          <div className={styles.data_item_row}>
+            <a 
+              className={styles.icon} 
+              href="https://maps.app.goo.gl/FuTkt3hptixiTBSw5" 
+              title="maps_location" 
+              target="_blank"
+              >
+              <MapPinSvg />
+            </a>
+            <h3 className={styles.title}>Ubicanos</h3>
+          </div>
+          <div className={styles.data_item_col}>
             <p>Av Leandro N Alem 862,</p>
             <p>Bahía Blanca CP 8000,</p>
-            <p>LoBuenos Airesem</p>
+            <p>Buenos Aires</p>
           </div>
         </FramerReveal>
 
@@ -103,16 +105,18 @@ export const _09Contacto = () => {
           className={styles.data_item}
           options={{from: "bottom", amount:"60", margin: "-20", delay: "0.25"}}
         >
-          <a 
-            className={styles.icon} 
-            href="https://wa.me/+5492914054505" 
-            title="numero_telefonico" 
-            target="_blank"
-          >
-            <PhoneSvg />
-          </a>
-          <h3 className={styles.title}>Llamanos</h3>
-          <div>
+          <div className={styles.data_item_row}>
+            <a 
+              className={styles.icon} 
+              href="https://wa.me/+5492914054505" 
+              title="numero_telefonico" 
+              target="_blank"
+            >
+              <PhoneSvg />
+            </a>
+            <h3 className={styles.title}>Llamanos</h3>
+          </div>
+          <div className={styles.data_item_col}>
             <p>
               <a 
               href="https://wa.me/+5492914054505" 
@@ -129,24 +133,26 @@ export const _09Contacto = () => {
           className={styles.data_item}
           options={{from: "bottom", amount:"60", margin: "-20", delay: "0.5"}}
         >
-          <a 
-            className={styles.mailto_icon_desktop} 
-            href="https://mail.google.com/mail/u/0/?fs=1&to=info@puntoalemeventos.com&su=&body=&bcc=&tf=cm" 
-            title="mail_de_la_empresa" 
-            target="_blank"
-          >
-            <MailSvg />
-          </a>
-          <a 
-            className={styles.mailto_icon_movil} 
-            href="mailto:info@puntoalemeventos.com" 
-            title="mail_de_la_empresa" 
-            target="_blank"
-          >
-            <MailSvg />
-          </a>
-          <h3 className={styles.title}>Escribinos</h3>
-          <div>
+          <div className={styles.data_item_row_mail}>
+            <a 
+              className={styles.mailto_icon_desktop} 
+              href="https://mail.google.com/mail/u/0/?fs=1&to=info@puntoalemeventos.com&su=&body=&bcc=&tf=cm" 
+              title="mail_de_la_empresa" 
+              target="_blank"
+            >
+              <MailSvg />
+            </a>
+            <a 
+              className={styles.mailto_icon_movil} 
+              href="mailto:info@puntoalemeventos.com" 
+              title="mail_de_la_empresa" 
+              target="_blank"
+            >
+              <MailSvg />
+            </a>
+            <h3 className={styles.title}>Escribinos</h3>
+          </div>
+          <div className={styles.data_item_col}>
             <p>
               <a 
                 className={styles.mailto_desktop}
@@ -172,27 +178,29 @@ export const _09Contacto = () => {
           className={styles.data_item}
           options={{from: "bottom", amount:"60", margin: "-20", delay: "0.75"}}
         >
-          <div className={styles.socials}>
-            <a 
-              className={styles.icon} href="https://www.instagram.com/punto.alem/" 
-              title="instagram" 
-              target="_blank"
-            >
-              <InstagramSvg />
-            </a>
-            <a 
-              className={styles.icon} 
-              href="https://www.facebook.com/Puntoalemeventos" 
-              title="facebook" 
-              target="_blank"
-            >
-              <FacebookSvg 
-              className={styles.facebook_svg}
-              />
-            </a>
+          <div className={styles.data_item_row}>
+            <div className={styles.socials}>
+              <a 
+                className={styles.icon} href="https://www.instagram.com/punto.alem/" 
+                title="instagram" 
+                target="_blank"
+              >
+                <InstagramSvg />
+              </a>
+              <a 
+                className={styles.icon} 
+                href="https://www.facebook.com/Puntoalemeventos" 
+                title="facebook" 
+                target="_blank"
+              >
+                <FacebookSvg 
+                className={styles.facebook_svg}
+                />
+              </a>
+            </div>
+            <h3 className={styles.title}>Seguinos</h3>
           </div>
-          <h3 className={styles.title}>Seguinos</h3>
-          <div>
+          <div className={styles.data_item_col}>
             <p>
               <a 
                 href="https://www.instagram.com/punto.alem/" 
